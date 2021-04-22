c763b9b1fec5ec661bf4f48bc4ad70caaaf200be
dejayru <rkpmobile@gmail.com>
2021-04-22 17:13:01 -0400

    vacationPy start

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "API Key Here"
+weather_api_key = "5d90907cfb6f3154c1d4dfb218543bed"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyCZ0wLyzP3QjMELiTKuINlmkoKFIOrykuo"
